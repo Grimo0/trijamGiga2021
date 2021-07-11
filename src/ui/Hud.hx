8f3ae529771db34548bd67be5fd980e0af430370
Grimo0 <gregoir.andre@gmail.com>
2021-07-11 19:05:48 +0200

    Update Hud base

--- src/ui/Hud.hx
+++ src/ui/Hud.hx
@@ -1,37 +1,17 @@
 package ui;
 
 class Hud extends dn.Process {
-	public var game(get, never) : Game;
-	inline function get_game() return Game.ME;
-
-	var flow : h2d.Flow;
-	var invalidated = true;
+	public var game(get, never) : Game; inline function get_game() return Game.ME;
 
 	public function new() {
 		super(Game.ME);
 
 		createRootInLayers(game.root, Const.MAIN_LAYER_UI);
 		root.filter = new h2d.filter.ColorMatrix(); // force pixel perfect rendering
-
-		flow = new h2d.Flow(root);
 	}
 
 	override function onResize() {
 		super.onResize();
 		root.setScale(Const.UI_SCALE);
-	}
-
-	public inline function invalidate()
-		invalidated = true;
-
-	function render() {}
-
-	override function postUpdate() {
-		super.postUpdate();
-
-		if (invalidated) {
-			invalidated = false;
-			render();
-		}
 	}
 }
